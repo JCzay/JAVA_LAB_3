05fdd261004161a2b857e6f5c93d90885def2eca
Julian <Julian.Czaja99@icloud.com>
2022-02-04 18:01:17 +0100

    Ćwiczenie 1.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,54 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw 1 enum-y */
+        System.out.println("Podaj liczbę: ");
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            int number = scan.nextInt();
+            switch (number) {
+                case 0:
+                    System.out.println("Podana przez Ciebie liczba to 0 więc program zostanie zakończony! " + StatusEnum.KONIEC);
+                    return;
+                case 1:
+                    System.out.println("Słowny zapis liczby: " + LiczbyEnum.JEDEN);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+                case 2:
+                    System.out.println("Słowny zapis liczby: " + LiczbyEnum.DWA);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+                case 3:
+                    System.out.println("Słowny zapis liczby: " + LiczbyEnum.TRZY);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+                case 4:
+                    System.out.println("Słowny zapis liczby: " + LiczbyEnum.CZTERY);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+                case 5:
+                    System.out.println("Słowny zapis liczby: " + LiczbyEnum.PIEC);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+                case 6:
+                    System.out.println("Słowny zapis liczby: " + LiczbyEnum.SZESC);
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+                default:
+                    System.out.println(StatusEnum.KONTYNUUJEMY);
+                    break;
+            }
+        }
 
-        /* zad.1
-        a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
-        b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
-        c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
-        d) niech w pętli są podawane wartości liczbowe z klawiatury,
-        e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
-        f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
-        do podanych cyfr przez użytkownika,
-        g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
-        */
+    }
 
-        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
-        * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
-
+    enum LiczbyEnum {
+        JEDEN, DWA, TRZY, CZTERY, PIEC, SZESC
+    }
+    enum StatusEnum {
+        KONTYNUUJEMY, KONIEC
     }
 }
